9ca2a893e30591e4767ee486265dd103c6aac350
Pierre Gozard <pierre.gozard03@gmail.com>
2026-03-13 16:09:14 +0100

    navbar amélioré

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!
 
   def home
+    @poles = Pole.ordered
+    @news = News.recent
+  end
+
+  def patients
+  end
+
+  def professionnels
   end
 end
